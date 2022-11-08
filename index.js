e3aad4e283f19b963183c07496864e56d987b871
AdnanNadeem0923 <adnannadeem0923@gmail.com>
2022-11-08 03:59:09 -0800

    first commit

--- index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(9000,() =>{
-    console.log("Array of states at http://localhost:9000");
-});
---
+++ samples.js
@@ -0,0 +1,5 @@
+// const app = require("./app");
+
+// app.listen(9000,() =>{
+//     console.log("Array of states at http://localhost:9000");
+// }); 
